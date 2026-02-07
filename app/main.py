77ea5d8ca2ea952628d84861da0eb3c544ae12a2
Kott Q <constantinekuncel@gmail.com>
2026-02-07 21:46:40 +0100

    final

--- app/main.py
+++ app/main.py
@@ -28,15 +28,13 @@ class CarWashStation:
         self.count_of_ratings = count_of_ratings
 
     def calculate_washing_price(self, car: Car) -> float:
-        result = round(
-            (car.comfort_class
-             * (self.clean_power - car.clean_mark)
-             * self.average_rating
-             / self.distance_from_city_center
-             ), 1
+        price = (
+                car.comfort_class
+                * (self.clean_power - car.clean_mark)
+                * self.average_rating
+                / self.distance_from_city_center
         )
-        print(result)
-        return result
+        return round(price, 1)
 
     def wash_single_car(self, car: Car) -> None:
         if car.clean_mark < self.clean_power:
